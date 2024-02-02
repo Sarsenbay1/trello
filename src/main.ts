6f1cedb9854570dcf0037ab03b89e65c0399e7b7
maksat sarsenbaev <88890025m@gmail.com>
2024-02-02 17:24:20 +0600

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
-  await app.listen(process.env.PORT || 3000);
-  console.log(`PORT ${process.env.PORT || 3000}`);
+  const config = new DocumentBuilder()
+    .setTitle('Trello')
+    .setDescription('The trello API')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.PORT || 5000);
+  console.log(`server ok PORT ${process.env.PORT || 5000}`);
 }
 bootstrap();
